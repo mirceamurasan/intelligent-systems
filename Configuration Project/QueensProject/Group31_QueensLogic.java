371397e3596c2f047fc04c8d4522d51b745be061
PeterOeClausen <PeterOeClausen@gmail.com>
2018-04-03 18:41:35 +0200

    Some comments

--- Configuration Project/QueensProject/Group31_QueensLogic.java
+++ Configuration Project/QueensProject/Group31_QueensLogic.java
@@ -14,10 +14,10 @@ public class Group31_QueensLogic implements IQueensLogic{
     private int[][] board;	// Content of the board. Possible values: 0 (empty), 1 (queen), -1 (no queen allowed).
 	private BDD rules; 		// A BDD representing all the rules of the game.
 	private BDDFactory factory; //A factory that makes it easy to interact with the BDD.
-	private int queensPlaced;	
+	private int queensPlaced;	//Number of queens placed, used for auto finnish when there are no choices left.
 
 	/**
-	 *
+	 * Initializes the board and the BDD.
 	 * @param size The size of the board ( i.e. size = #rows = #columns)
 	 */
 	public void initializeBoard(int size) {
@@ -33,12 +33,20 @@ public class Group31_QueensLogic implements IQueensLogic{
         generateBdd();
     }
 
-    public int[][] getBoard() {
+	/**
+	 * Returns the board in it's current state.
+	 */
+	public int[][] getBoard() {
         return board;
     }
-	
+
+	/**
+	 * Generates the rules for the NQueens game and updates the BDD.
+	 * We have the rule that we need one Queen per row (to finnish the game)
+	 * Also we need
+	 */
 	public void generateBdd(){
-		generateOneQueenPerRowRule();
+		generateOneQueenPerColumnRule();
 		
 		for(int col=0;col<size;col++){
 			for(int row=0;row<size;row++){
@@ -49,7 +57,25 @@ public class Group31_QueensLogic implements IQueensLogic{
 			}
 		}
 	}
-	
+
+	/**
+	 * Adds one rule per column, saying that only one queen can be placed per column.
+	 */
+	public void generateOneQueenPerColumnRule(){
+		for(int col = 0 ; col < size; col++){
+			BDD temp = factory.zero(); 				// one bdd for each column.
+			for(int row = 0; row < size; row++){
+				int varNum = getVariable(col,row);
+				temp.orWith(factory.ithVar(varNum));// Only one queen can be placed for this column.
+			}
+			rules.andWith(temp); // Add the rule for this column to the BDD of global rules.
+		}
+	}
+
+	/**
+	 * Adds rule saying that if a queen is placed on position (column,row) then we can't place a queen on any other
+	 * column position in the same row.
+	 */
 	public void generateRowRule(int column, int row){
 		BDD temp = factory.one(); // we use one because we will use the and operator (one being neutral with and)
 		int current = getVariable(column,row);
@@ -63,7 +89,10 @@ public class Group31_QueensLogic implements IQueensLogic{
 		rules.andWith(newRule);
 	}
 
-	// [Column, Row] [i, j]
+	/**
+	 * Adds rule saying that if a queen is placed on position (column,row) then we can't place a queen on any other
+	 * row position in the same column.
+	 */
 	public void generateColumnRule(int column, int row){
 		BDD temp = factory.one(); // we use one because we will use the and operator (one being neutral with and)
 		int current = getVariable(column,row);
@@ -73,10 +102,14 @@ public class Group31_QueensLogic implements IQueensLogic{
 				temp.andWith(factory.nithVar(getVariable(column, r)));
 			}
 		}
-		BDD newRule = currentCell.imp(temp); //
+		BDD newRule = currentCell.imp(temp);
 		rules.andWith(newRule);
 	}
-	
+
+	/**
+	 * Adds a rule for position (column,row), saying that we can't place queens in the same diagonal
+	 * (top left to bottom right) diagonal as this position.
+	 */
 	public void generateDiagonalRuleOne(int column, int row){
 		BDD temp = factory.one();
 		int current = getVariable(column,row);
@@ -97,56 +130,52 @@ public class Group31_QueensLogic implements IQueensLogic{
 		BDD newRule = currentCell.imp(temp); //
 		rules.andWith(newRule);
 	}
-	
-	public void generateDiagonalRuleTwo(int i, int j){
-		BDD temp=factory.one();
-		int current=getVariable(i,j);
-		BDD currentCell=factory.ithVar(current);
 
-		int x = j - size;
-		int y = i + size;
+	/**
+	 * Adds a rule for position (column, row), saying that we can't place queens in the same diagonal
+	 * (top right to bottom left) diagonal as this position.
+	 */
+	public void generateDiagonalRuleTwo(int column, int row){
+		BDD temp = factory.one();
+		int current = getVariable(column,row);
+		BDD currentCell = factory.ithVar(current);
+
+		int x = row - size;
+		int y = column + size;
 		while (x < size && y >= 0) {
-			if (x >= 0 && y >= 0 && x < size && y < size) {
-				if(x!=j && y!=i){
+			if (x >= 0 && y < size) {
+				if(x != row && y != column){
 					temp.andWith(factory.nithVar(getVariable(y,x)));
 				}
 			}
-
 			x++;
 			y--;
 		}
-		BDD newRule = currentCell.imp(temp); //
+		BDD newRule = currentCell.imp(temp);
 		rules.andWith(newRule);
 	}
-	
-	public void generateOneQueenPerRowRule(){
-		for(int i=0;i<size;i++){
-			BDD temp=factory.zero(); // one bdd for each row
-			for(int j=0;j<size;j++){ // it can only be one of the cells on each row
-				int var=getVariable(i,j);
-				temp.orWith(factory.ithVar(var));// that's why we use or
-			}
-			rules.andWith(temp); // add the rule for this row to the global rules
-			
-		}
-	}
-	
-	 private int getVariable(int column, int row) {
+
+	/**
+	 * Returns the variable number a given column and row.
+	 */
+	private int getVariable(int column, int row) {
         return row * this.size + column;
     }
 
-    public void insertQueen(int column, int row)
+	/**
+	 * Inserts a queen in position (column,row), updates the BDD and the board based on the BDD.
+	 * It also checks if it can autocomplete, and does so if it can.
+	 */
+	public void insertQueen(int column, int row)
     {
-    	// Need to add constraint to the BDD (Column, Row)
-		// Need to update Board with getVariable for all positions
         if (isValidPlacement(column, row)) {
-			board[column][row] = 1;
+			//Assigns true to the variable on position (column,row):
 			rules.restrictWith(factory.ithVar(getVariable(column, row)));
-
-			//factory.ithVar(getVariable(column, row)).restrictWith(factory.one());
+			board[column][row] = 1;
 
 			for (int col = 0; col < size; col++) {
 				for (int rov = 0; rov < size; rov++) {
+					//Checks if the BDD evaluates to false if placing a queen on (col,rov):
 					if (rules.restrict(factory.ithVar(getVariable(col,rov))).isZero()) {
 						board[col][rov] = -1;
 					}
@@ -159,7 +188,11 @@ public class Group31_QueensLogic implements IQueensLogic{
         }
     }
 
-    private boolean canAutoComplete(){
+	/**
+	 * Returns true if number of queens placed + number of queens is equal to the N.
+	 * In other words: If the user has no more choices to place queens.
+	 */
+	private boolean canAutoComplete(){
 		int numMissingQueens = 0;
 		for(int col = 0; col < size; col++){
 			for(int row = 0; row < size; row++){
@@ -174,10 +207,14 @@ public class Group31_QueensLogic implements IQueensLogic{
 			return false;
 	}
 
+	/**
+	 * Checks where there are missing queens, updates the BDD (to show crosses) and the board to show queens.
+	 */
 	private void autoComplete(){
 		for(int col = 0; col < size; col++){
 			for(int row = 0; row < size; row++){
 				if(getBoard()[col][row] == 0){
+					//Assigns true to the variable on position (col,row):
 					rules.restrictWith(factory.ithVar(getVariable(col, row)));
 					board[col][row] = 1;
 				}
@@ -185,42 +222,9 @@ public class Group31_QueensLogic implements IQueensLogic{
 		}
 	}
 
-    private void markIllegalMoves(int column, int row)
-    {
-        // Horizontal
-        for (int i = 0; i < size; i++) {
-            board[column][i] = -1;
-        }
-
-        // Vertical
-        for (int i = 0; i < size; i++) {
-            board[i][row] = -1;
-        }
-
-        // Diagonal (left top -> bottom right)
-        int x = row - size;
-        int y = column - size;
-        while (x < size && y < size) {
-            if (x >= 0 && y >= 0 && x < size && y < size) {
-                board[y][x] = -1;
-            }
-
-            x++;
-            y++;
-        }
-
-        x = row - size;
-        y = column + size;
-        while (x < size && y >= 0) {
-            if (x >= 0 && y >= 0 && x < size && y < size) {
-                board[y][x] = -1;
-            }
-
-            x++;
-            y--;
-        }
-    }
-
+	/**
+	 * Returns true if I can place a queen on a given position (column,row).
+	 */
     private boolean isValidPlacement(int column, int row)
     {
         return board[column][row] == 0;
